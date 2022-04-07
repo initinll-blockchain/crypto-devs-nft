8c4984c86fc043e13068bf2bfb2841b8e7d62b1b
Nitin Londhe <nitin.londhe@genmills.com>
2022-04-08 02:55:44 +0530

    added contract address

--- front-end/src/lib/helpers/Constants.ts
+++ front-end/src/lib/helpers/Constants.ts
@@ -1,4 +1,4 @@
 export class Constants {
     static readonly CONTRACT_ADDRESS_LOCALHOST: string = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-    static readonly CONTRACT_ADDRESS_RINKEBY: string = '';
+    static readonly CONTRACT_ADDRESS_RINKEBY: string = '0x123a7ef5a0107e04Cc2F6af39dBE43c44F5dC4a4';
 }
